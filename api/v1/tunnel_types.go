38b2df042d74c1b0ceb2412a06e077981812df18
laetho <thorsplass@gmail.com>
2021-06-17 21:21:42 +0200

    WIP Added the Tunnel api.

---
+++ api/v1/tunnel_types.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2021 The Argonaut authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1
+
+import (
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// TunnelSpec defines the desired state of Tunnel.
+type TunnelSpec struct {
+	// +kubebuilder:validation:MinLength=0
+
+	// CloudFlare AccountTag
+	AccountTag string `json:"accountTag"`
+
+	// UUID of the tunnel
+	TunnelId string `json:"tunnelId"`
+
+	// Reference to secret holding the Tunnel Secret
+	TunnelSecret v1.SecretReference `json:"tunnelSecret"`
+
+	// Reference to a secret containing the cert.pem coined when creating a tunnel.
+	Cert v1.SecretReference `json:"cert"`
+}
+
+// TunnelStatus defines the observed state of Tunnel
+type TunnelStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// Tunnel is the Schema for the tunnels API
+type Tunnel struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   TunnelSpec   `json:"spec,omitempty"`
+	Status TunnelStatus `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// TunnelList contains a list of Tunnel
+type TunnelList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Tunnel `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Tunnel{}, &TunnelList{})
+}
